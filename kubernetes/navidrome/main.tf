67f811afca3a9a53faab76cd87b759dd3f33c3ac
misogihagi <47350059+misogihagi@users.noreply.github.com>
2026-05-30 09:41:55 +0900

    feat: add navidrome

---
+++ kubernetes/navidrome/main.tf
@@ -0,0 +1,144 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = var.kube_config_path
+  }
+}
+
+provider "kubernetes" {
+  config_path = var.kube_config_path
+}
+
+resource "kubernetes_namespace" "navidrome" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+locals {
+  namespace = kubernetes_namespace.navidrome.metadata[0].name
+}
+
+resource "kubernetes_persistent_volume_claim" "config" {
+  metadata {
+    name      = "${var.release_name}-config"
+    namespace = local.namespace
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = var.config_size
+      }
+    }
+  }
+  wait_until_bound = false
+}
+
+resource "kubernetes_persistent_volume_claim" "music" {
+  metadata {
+    name      = "${var.release_name}-music"
+    namespace = local.namespace
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = var.music_size
+      }
+    }
+  }
+  wait_until_bound = false
+}
+
+resource "helm_release" "navidrome" {
+  name       = var.release_name
+  repository = "https://djjudas21.github.io/charts/"
+  chart      = "navidrome"
+  version    = var.chart_version
+
+  namespace = local.namespace
+
+  depends_on = [
+    kubernetes_persistent_volume_claim.config,
+    kubernetes_persistent_volume_claim.music,
+  ]
+
+  set {
+    name  = "env.TZ"
+    value = var.timezone
+  }
+
+  set {
+    name  = "env.ND_LOGLEVEL"
+    value = var.log_level
+  }
+
+  set {
+    name  = "env.ND_MUSICFOLDER"
+    value = "/music"
+  }
+
+  set {
+    name  = "env.ND_SESSIONTIMEOUT"
+    value = "24h"
+  }
+
+  set {
+    name  = "env.ND_ENABLETRANSCODINGCONFIG"
+    value = "true"
+  }
+
+  set {
+    name  = "persistence.config.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "persistence.config.existingClaim"
+    value = kubernetes_persistent_volume_claim.config.metadata[0].name
+  }
+
+  set {
+    name  = "persistence.music.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "persistence.music.existingClaim"
+    value = kubernetes_persistent_volume_claim.music.metadata[0].name
+  }
+
+  set {
+    name  = "ingress.main.enabled"
+    value = tostring(var.ingress_enabled)
+  }
+
+  dynamic "set" {
+    for_each = var.ingress_enabled ? [1] : []
+    content {
+      name  = "ingress.main.hosts[0].host"
+      value = var.ingress_host
+    }
+  }
+
+  dynamic "set" {
+    for_each = var.ingress_enabled ? [1] : []
+    content {
+      name  = "ingress.main.hosts[0].paths[0].path"
+      value = "/"
+    }
+  }
+}
---
+++ kubernetes/navidrome/variables.tf
@@ -0,0 +1,59 @@
+variable "release_name" {
+  description = "The name of the Helm release"
+  type        = string
+  default     = "my-navidrome"
+}
+
+variable "namespace" {
+  description = "The namespace to install the release into"
+  type        = string
+  default     = "navidrome"
+}
+
+variable "chart_version" {
+  description = "The version of the Helm chart to install"
+  type        = string
+  default     = "6.8.3"
+}
+
+variable "kube_config_path" {
+  description = "Path to the kubeconfig file"
+  type        = string
+  default     = "/etc/rancher/k3s/k3s.yaml"
+}
+
+variable "config_size" {
+  description = "Storage size for the Navidrome config/data PVC"
+  type        = string
+  default     = "1Gi"
+}
+
+variable "music_size" {
+  description = "Storage size for the music library PVC"
+  type        = string
+  default     = "100Gi"
+}
+
+variable "timezone" {
+  description = "Timezone for the Navidrome container"
+  type        = string
+  default     = "Asia/Tokyo"
+}
+
+variable "log_level" {
+  description = "Log level for Navidrome (debug, info, warn, error)"
+  type        = string
+  default     = "info"
+}
+
+variable "ingress_enabled" {
+  description = "Enable ingress for Navidrome"
+  type        = bool
+  default     = false
+}
+
+variable "ingress_host" {
+  description = "Hostname for the Navidrome ingress (required if ingress_enabled = true)"
+  type        = string
+  default     = "navidrome.example.com"
+}
